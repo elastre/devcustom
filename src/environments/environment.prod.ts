2f7a6f60e67f8215e4d268f824197f13b49f839e
elastre <engells.lastre@gmail.com>
2018-06-11 19:37:49 -0500

    actualizacion FB

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig:{
+    apiKey: "AIzaSyC5n6rCY5-_FAon5Z5PKhWlr5g5lApOikI",
+    authDomain: "trzproyect.firebaseapp.com",
+    databaseURL: "https://trzproyect.firebaseio.com",
+    projectId: "trzproyect",
+    storageBucket: "trzproyect.appspot.com",
+    messagingSenderId: "831939222047"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseconfig:{
+    apiKey: "AIzaSyC5n6rCY5-_FAon5Z5PKhWlr5g5lApOikI",
+    authDomain: "trzproyect.firebaseapp.com",
+    databaseURL: "https://trzproyect.firebaseio.com",
+    projectId: "trzproyect",
+    storageBucket: "trzproyect.appspot.com",
+    messagingSenderId: "831939222047"
+  }
 };
 
 /*
